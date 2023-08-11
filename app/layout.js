10d9c1e7bf6bcab676a4a320a9e07619d786e1e8
JereUR <jeremias.jdv@gmail.com>
2023-08-11 03:17:54 -0300

    feat(Root): next/font dependencie added and used in body

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,11 @@
 import './globals.css'
+import { Space_Grotesk } from '@next/font/google'
 import Navigation from './components/Navigation'
+
+const font = Space_Grotesk({
+  subsets: ['latin', 'latin-ext', 'vietnamese'],
+  weight: ['400', '700']
+})
 
 export default function RootLayout({ children }) {
   return (
@@ -7,7 +13,7 @@ export default function RootLayout({ children }) {
       <head>
         <title>App with Next 13</title>
       </head>
-      <body>
+      <body className={font.className}>
         <Navigation />
         {children}
       </body>
