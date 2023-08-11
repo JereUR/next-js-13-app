79c84992715e74a65b40aa566fef9f46eb2866d8
JereUR <jeremias.jdv@gmail.com>
2023-08-11 03:37:44 -0300

    feat(Root): Image for user comment used

--- app/(white-banner)/posts/[id]/comments/page.jsx
+++ app/(white-banner)/posts/[id]/comments/page.jsx
@@ -1,5 +1,7 @@
+import Image from 'next/image'
+
 const fetchComments = async (id) => {
-  await new Promise((resolve) => setTimeout(resolve, 1000)) //Para visualizar loader
+  //await new Promise((resolve) => setTimeout(resolve, 1000))Para visualizar loader
 
   return fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`, {
     next: {
@@ -16,6 +18,12 @@ export default async function Comments({ params }) {
     <ul style={{ fontSize: '12px', background: '#444', padding: '10px 40px' }}>
       {comments.map((comment) => (
         <li key={comment.id}>
+          <Image
+            src={`https://avatars.dicebear.com/api/pixel-art-neutral/${comment.email}.svg`}
+            alt={comment.name}
+            width="30"
+            height="30"
+          />
           <h4>{comment.name}</h4>
           <small>{comment.body}</small>
         </li>
